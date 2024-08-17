389d6ef2f4bb0f60e749aee18c62a30e1b60c9d3
Mahesh15 <109548760+Maheshkumarjena@users.noreply.github.com>
2024-08-17 18:42:33 +0530

    signin and signup pages created

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// let browserWidth = window.innerWidth;
-// let browserHeight = window.innerHeight;
+console.log("script loaded")
 const sidebarHandler = () => {
     const element = document.getElementById('sideBar'); // Replace with your actual element
 const hasHiddenClass = element.classList.contains('hidden');
@@ -17,5 +16,53 @@ else if (hasFlexClass){
 };
 
 
+//  signUp js
 
-// SIDEBAR js
+let users = JSON.parse(localStorage.getItem('users')) || {};
+
+const signup = async () => {
+    event.preventDefault();
+    const email = await document.getElementById('email').value;
+    const password = await document.getElementById('password').value;
+
+    const user = {
+        email: email,
+        password: password
+    };
+
+    users[email] = user; // Assuming 'email' is unique for each user
+
+    localStorage.setItem('users', JSON.stringify(users));
+
+    console.log('User data stored:', users);
+
+}; 
+
+// const storedUser = JSON.parse(localStorage.getItem('users'));
+// console.log('Retrieved user data:', storedUser);
+
+
+
+console.log("users outside ", users )
+
+
+
+const handlelogin = async () => {
+    event.preventDefault();
+    const email = await document.getElementById('email').value;
+    const password = await document.getElementById('password').value;
+
+    const users = JSON.parse(localStorage.getItem('users'));
+
+    const user = users[email];
+    if(user){
+        if(user.password === password){
+            window.location.href = "../../Home.html";
+
+        }
+    }
+    else{
+        console.log("there is some error")
+    }
+
+}
